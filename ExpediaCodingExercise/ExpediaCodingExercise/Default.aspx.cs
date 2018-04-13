2b4421d22eb0275d79ce0d3cace83ce66cddd394
Marleen <marleenhaddad75@gmail.com>
2018-04-13 16:04:22 +0300

    Design of the project

---
+++ ExpediaCodingExercise/ExpediaCodingExercise/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExpediaCodingExercise
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnShowAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void dgvHotels_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+    }
+}
